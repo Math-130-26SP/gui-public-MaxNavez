69daaf8497c07038695420bd96deef4d03503621
MaxNavez <maximonavez@gmail.com>
2026-05-06 15:50:55 -0700

    Finished adding instance variables for some fields in my application

--- src/main/java/math130/gui/HelloController.java
+++ src/main/java/math130/gui/HelloController.java
@@ -24,6 +24,33 @@ public class HelloController {
     private TextField completedPasses;
     @FXML
     private TextField passPer;
+    @FXML
+    private TextField shots;
+    @FXML
+    private TextField shotsOnTarget;
+    @FXML
+    private TextField shotPer;
+    @FXML
+    private TextField dribbles;
+    @FXML
+    private TextField completedDribbles;
+    @FXML
+    private TextField dribblesPer;
+    @FXML
+    private TextField aerialDuels;
+    @FXML
+    private TextField aerialDuelsWon;
+    @FXML
+    private TextField aerialPer;
+    @FXML
+    private TextField tackles;
+    @FXML
+    private TextField succesfulTackles;
+    @FXML
+    private TextField tacklePer;
+    @FXML
+    private TextField matchImpactScore;
+
 
     @FXML
     protected void onEnter() {
@@ -35,6 +62,19 @@ public class HelloController {
         System.out.println(passes.getText());
         System.out.println(completedPasses.getText());
         System.out.println(passPer.getText());
+        System.out.println(shots.getText());
+        System.out.println(shotsOnTarget.getText());
+        System.out.println(shotPer.getText());
+        System.out.println(dribbles.getText());
+        System.out.println(completedDribbles.getText());
+        System.out.println(dribblesPer.getText());
+        System.out.println(aerialDuels.getText());
+        System.out.println(aerialDuelsWon.getText());
+        System.out.println(aerialPer.getText());
+        System.out.println(tackles.getText());
+        System.out.println(succesfulTackles.getText());
+        System.out.println(tacklePer.getText());
+        System.out.println(matchImpactScore.getText());
 
     }
 }
